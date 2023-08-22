bf2c04d7bfa4cff8bba1af65d9f7a1e671398bd3
Chandrucloudin36 <136332639+Chandrucloudin36@users.noreply.github.com>
2023-08-22 12:53:38 +0530

    on hme view cart

--- basedata/ErrorResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.cloudin.monsterchicken.basedata
-
-import com.google.gson.annotations.SerializedName
-
-data class ErrorResponse(
-    @SerializedName("message") val errorResponseList: List<String>
-)
